53ee2ff2e1d8982fe79fb6de7cfbb9db6bc03326
Chibuzo <chibuzo.henry@gmail.com>
2021-09-10 11:08:41 +0100

    Update config

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
     "development": {
         "username": 'root',
-        "password": '',
-        "database": 'farm',
+        "password": process.env.DEV_DB_PASSWORD,
+        "database": 'farm_dev',
         "host": '127.0.0.1',
         "dialect": "mysql",
         //"operatorsAliases": false
