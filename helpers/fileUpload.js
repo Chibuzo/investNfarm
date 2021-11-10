86ffb7cd6512476897e681385705bdbcc29d9439
Chibuzo <chibuzo.henry@gmail.com>
2021-11-10 09:38:00 +0100

    Minor ui fixes

--- helpers/fileUpload.js
+++ helpers/fileUpload.js
@@ -8,8 +8,8 @@ const uploadFile = async files => {
     // check file type
     const ext = files.portfolio_photo.name.split('.').pop();
     const photoFile = files.portfolio_photo;
-    const photoName = `public/portfolio_photos/${process.hrtime()[1]}.${ext}`;
-    const uploadPath = require('path').resolve(__dirname, '../', photoName);
+    const photoName = `portfolio_photos/${process.hrtime()[1]}.${ext}`;
+    const uploadPath = require('path').resolve(__dirname, '../public', photoName);
     await photoFile.mv(uploadPath);
     return photoName;
 }
